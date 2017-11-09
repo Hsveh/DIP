647f88af721947f458ef4bceb923c3391c39287a
Hsveh <xfysjtu@gmail.com>
2017-11-09 21:22:13 +0800

    problem 8 :finish Opening by reconstruction
    problem 8 :finish Opening by reconstruction

---
+++ Solutions/problem7.m
@@ -0,0 +1,95 @@
+%{
+%}
+image = imread('/Users/xuefanyong/Documents/GitHub/DIP/Solutions/images/lenna.tif');
+image = im2double(image);
+zonal_mask = [1 1 1 1 1 ;1 1 1 1 0;1 1 1 0 0;1 1 0 0 0;1 0 0 0 0];
+zonal_mask = [zonal_mask zeros(5,3)];
+zonal_mask = [zonal_mask;zeros(3,8)];
+
+threshold_mask = [1 1 0 1 1 0 0 0;1 1 1 1 0 0 0 0;1 1 0 0 0 0 0 0;1 0 0 0 0 0 0 0];
+threshold_mask = [threshold_mask;zeros(1,8);0 1 0 0 0 0 0 0;zeros(2,8)];
+
+figure();
+imshow(haar(image,1));
+
+function image_p = get_part(image,part)
+    [row,col] = size(image);
+    if part == 1
+        image_p = image(1:row/2,1:col/2);
+    elseif part == 2
+        image_p = image(1:row/2,col/2+1:col);
+    elseif part == 3
+        image_p = image(row/2+1:row,1:col/2);
+    elseif part == 4
+        image_p = image(row/2+1:row,col/2+1:col);
+    end
+    
+end
+function image_ = haar(image,round)
+    [row,col] = size(image);
+    image_ = image;
+    
+    for r = 1:round
+        image_(1:row/(2^r),1:col/(2^r)) = (get_part(image,1)+get_part(image,2)+get_part(image,3)+get_part(image,4))/2;
+        image_(1:row/(2^r),col/(2^r)+1:col/(2^(r-1))) = (get_part(image,1)-get_part(image,2)+get_part(image,3)-get_part(image,4))/2;
+        image_(row/(2^r)+1:row/(2^(r-1)),1:col/(2^r)) = (get_part(image,1)+get_part(image,2)-get_part(image,3)-get_part(image,4))/2;
+        image_(row/(2^r)+1:row/(2^(r-1)),col/(2^r)+1:col/(2^(r-1))) = (get_part(image,1)-get_part(image,2)-get_part(image,3)+get_part(image,4))/2;
+        image = image_(1:row/(2^r),1:col/(2^r));
+    end
+    
+end
+
+
+function m = get_matrix(n)
+    m = zeros(n);
+    for u = 0:n-1
+        for x = 0:n-1
+            m(u+1,x+1)=alpha(u,n)*cos(((2*x+1)*u*pi)/(2*n));
+        end
+    end
+end
+
+function image_ = DCT(image,size_,mask)
+    image = convert(image,size_);
+    [row,col]=size(image);
+    image_ = im2double(zeros(row,col));
+    
+    m = get_matrix(size_);
+    
+    for r = 1:size_:row+1-size_
+        for c = 1:size_:col+1-size_
+            image_(r:r+size_-1,c:c+size_-1) = m*image(r:r+size_-1,c:c+size_-1)*m';
+        end
+    end
+    
+    for r = 1:size_:row+1-size_
+        for c=1:size_:col+1-size_
+            image_(r:r+size_-1,c:c+size_-1) = image_(r:r+size_-1,c:c+size_-1).*mask;
+        end
+    end
+end
+function image_ = IDCT(image,size_)
+    [row,col]=size(image);
+    image_ = im2double(zeros(row,col));
+    m = get_matrix(size_);
+    for r = 1:size_:row+1-size_
+        for c = 1:size_:col+1-size_
+            image_(r:r+size_-1,c:c+size_-1) = m'*image(r:r+size_-1,c:c+size_-1)*m;
+        end
+    end
+end
+function v = alpha(u,size_)
+    if u == 0
+        v = sqrt(1/size_);
+    else
+        v = sqrt(2/size_);
+    end
+end
+function image_ = convert(image,size_)
+    [row,col]=size(image);
+    size_x = mod(row,size_);
+    size_y = mod(col,size_);
+    image_ = [image zeros(row,size_y)];
+    image_ = [image_;zeros(size_x,col+size_y)];
+    image_ = im2double(image_);
+end
---
+++ Solutions/problem8.m
@@ -0,0 +1,85 @@
+image = imread('/Users/xuefanyong/Documents/GitHub/DIP/Solutions/images/Fig0929(a)(text_image).tif');
+figure();
+imshow(image);
+
+b=erode(image,51,1);
+figure();
+imshow(b);
+c = dilate(b,51,1);
+figure();
+imshow(c);
+
+d = rd(b,image);
+figure();
+imshow(d);
+function image_ = dilate(image,mask_size_x,mask_size_y)
+    [row,col] = size(image);
+    %{
+    image_ = zeros(row,col);
+    
+    image = [zeros(row,floor(mask_size_y/2)) image zeros(row,floor(mask_size_y/2))];
+    image = [zeros(floor(mask_size_x/2),col+mask_size_y-1);image;zeros(floor(mask_size_x/2),col+mask_size_y-1)];
+
+    for r = 1:row
+        for c = 1:col
+            image_(r,c) = logical(max(max(image(r:r+mask_size_x-1,c:c+mask_size_y-1))));
+        end
+    end
+    %}
+    image_ = zeros(row+mask_size_x-1,col+mask_size_y-1);%append
+    for r = 1:row
+        for c = 1:col
+            if image(r,c)
+                image_(r:r+mask_size_x-1,c:c+mask_size_y-1) = ones(mask_size_x,mask_size_y);
+            end
+        end
+    end
+    image_ = logical(image_(ceil(mask_size_x/2):row+floor(mask_size_x/2),ceil(mask_size_y/2):col+floor(mask_size_y/2)));
+end
+
+function image_ = erode(image,mask_size_x,mask_size_y)
+    [row,col] = size(image);
+    image_ = zeros(row,col);
+    
+    image = [ones(row,floor(mask_size_y/2)) image ones(row,floor(mask_size_y/2))];
+    image = [ones(floor(mask_size_x/2),col+mask_size_y-1);image;ones(floor(mask_size_x/2),col+mask_size_y-1)];
+
+    for r = 1:row
+        for c = 1:col
+            image_(r,c) = logical(min(min(image(r:r+mask_size_x-1,c:c+mask_size_y-1))));
+        end
+    end
+end
+
+function image_ = rd(F,G)
+    %figure();
+    pre_DGF = logical(and_image(dilate(F,3,3),G));
+    DGF = logical(and_image(dilate(pre_DGF,3,3),G));
+    while image_different(pre_DGF,DGF)
+        pre_DGF = DGF;
+        DGF = logical(and_image(dilate(pre_DGF,3,3),G));
+        %imshow(DGF);
+    end
+    image_ = DGF;
+    %close(figure());
+end
+
+function image_ = re(F,G)
+    pre_DGF = logical(or_image(erode(F,3,3),G));
+    DGF = logical(or_image(erode(pre_DGF,3,3),G));
+    while image_different(pre_DGF,DGF)
+        pre_DGF = DGF;
+        DGF = logical(and_image(erode(pre_DGF,3,3),G));
+    end
+    image_ = DGF;
+end
+
+function image_ = and_image(image1,image2)
+    image_ = image1&image2;
+end
+function image_ = or_image(image1,image2)
+    image_ = image1|image2;
+end
+function v = image_different(image1,image2)
+    v = sum(sum(image1-image2));
+end
