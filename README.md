2ea519e7a09fe064cae722d532fa77bd9254b53a
zhongbaicai <xfysjtu@163.com>
2017-12-21 20:54:18 +0800

    finish problem 5

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ My homework of Digital Image Processing.
 - [x] Problem2
 - [x] Problem3
 - [x] Problem4
-- [ ] Problem5
+- [x] Problem5
 - [x] Problem6
 - [ ] Problem7
 - [x] Problem8
@@ -23,9 +23,21 @@ My homework of Digital Image Processing.
 - [x] Problem2
 - [x] Problem3
 - [x] Problem4
-- [ ] Problem5
+- [x] Problem5
 - [x] Problem6
 - [ ] Problem7
 - [x] Problem8
 - [x] Problem9
+- [ ] Problem10
+
+### Check
+- [ ] Problem1
+- [ ] Problem2
+- [ ] Problem3
+- [ ] Problem4
+- [ ] Problem5
+- [ ] Problem6
+- [ ] Problem7
+- [ ] Problem8
+- [ ] Problem9
 - [ ] Problem10
--- Solutions/problem5.m
+++ Solutions/problem5.m
@@ -1,49 +1,104 @@
 %{
-
+    Problem 5
+    by Fanyong Xue
+	Student ID:515030910443
+    Image restoration
 %}
+%% Main Part
+
 clear all
 image = imread('/Users/xuefanyong/Documents/GitHub/DIP/Solutions/images/book_cover.jpg');
 image = im2double(image);
+
+% motion blur
 [row,col] = size(image);
+image_f = fft2(image,row,col);
+image_f2 = fftshift(image_f);
+fliter = blurring_degradation(row,col,0.1,0.1,1);
+image_t = image_f2.*fliter;
+G = image_t;
+image_t = ifftshift(image_t);
+image_t = ifft2(image_t);
+image_t = abs(image_t);
+image_t = im2uint8(image_t);
 
-image_f = fft2(image);
-h=blurring_degradation(row,col,0.1,0.1,1);
-k=h.*image_f;
-image_t = ifft2(k);
-image_i=wiener_filter(k,h,0);
-figure,imshow(image_i,[]);
+% add noise
+noise=gaussian_noise(row,col,0,25);
+noise=int16(noise);
+image_n = noise+int16(image_t);
+image_n = uint8(image_n);
+noise=uint8(noise);
 
-image__ = double(im2uint8(abs(image_t))) + gaussian_noise(row,col,0,2);
+% diaplsy
 figure();
-imshow(image__,[]);
-%{
-subplot(131);
+subplot(221);
 imshow(image);
-filter = blurring_degradation(2*row,2*col,0.1,0.1,1);
-image_ = transfer(image,filter);
-subplot(132);
-imshow(image_,[]);
+title('Original Image');
+subplot(222);
+imshow(image_t);
+title('Blurred Image');
+subplot(223);
+imshow(noise);
+title('Noise');
+subplot(224);
+imshow(image_n);
+title('Blurred Image with Noise');
 
+% restoration
+% no noise
+image_i = inverse_filter(G,fliter);
+figure();
+imshow(image_i);
+title('Inverse Filter');
 
-%}
+% with noise 650
+N = fft2(im2double(noise));
+N = fftshift(N);
+G_ = G+N;
+image_i2 = inverse_filter(G_,fliter);
+image_w = wiener_filter(G_,fliter,0.03);
+% display
+figure();
+subplot(121);
+imshow(image_i2,[]);
+title('Inverse Filter(650)');
+subplot(122);
+imshow(image_w);
+title('Wiener Filter(650)');
+
+%with noise 324
+noise=gaussian_noise(row,col,0,18);
+noise=uint8(noise);
+N = fft2(im2double(noise));
+N = fftshift(N);
+G_ = G+N;
+
+image_i2 = inverse_filter(G_,fliter);
+image_w = wiener_filter(G_,fliter,0.01);
+
+% display
+figure();
+subplot(121);
+imshow(image_i2,[]);
+title('Inverse Filter(324)');
+subplot(122);
+imshow(image_w);
+title('Wiener Filter(324)');
+% 
+%% Function Part
+% blurring degradation
 function h = blurring_degradation(row,col,a,b,T)
+    
     h = double(zeros(row,col));
     for u  = 1:row
         for v = 1:col
-            h(u,v) = T/(pi*(u*a+v*b))*sin(pi*(u*a+v*b))*exp(-1i*pi*(u*a+v*b));
+            if u +v == col
+                h(u,v)=1;
+                continue;
+            end
+            h(u,v) = T/(pi*((u-row/2)*a+(v-col/2)*b))*sin(pi*((u-row/2)*a+(v-col/2)*b))*exp(-1i*pi*((u-row/2)*a+(v-col/2)*b));
         end
     end
-end
-
-function image_t=transfer(image,fliter)
-    [row,col]=size(image);
-    image_f = fft2(image,2*row,2*col);
-    image_f = fftshift(image_f);
-    image_t = image_f.*fliter;
-    image_t = ifftshift(image_t);
-    image_t = ifft2(image_t);
-    image_t = image_t(1:row,1:col);
-    image_t = abs(image_t);
 end
 
 % gaussian noise
@@ -67,14 +122,17 @@ function n = gaussian_noise(row,col,ex,sigma)
     n = n*double(sigma);
     n = n+ex;
 end
+% inverse filter
 function image_ = inverse_filter(g,h)
     image_ = g./h;
+    image_ = ifftshift(image_);
     image_ = ifft2(image_);
     image_ = abs(image_);
 end
-
+% wiener filter
 function image_ = wiener_filter(g,h,k)
     image_ = ((conj(h).*h)./((conj(h).*h)+k)).*g./h;
+    image_ = ifftshift(image_);
     image_ = ifft2(image_);
     image_ = abs(image_);
 end
